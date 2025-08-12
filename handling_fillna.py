8d69ae568b117cc6c1feabb15253bf6a1d108955
Sandesh Raj Bhatta <106328473+Sandesh567@users.noreply.github.com>
2025-08-12 17:56:41 +0545

    Add files via upload
    Handling missing values by filling it rather than deleting it using fillna() method. Mean can be used or default value 0 can be passed.

---
+++ handling_fillna.py
@@ -0,0 +1,28 @@
+#Handling missing values by filling it rather than deleting it.
+
+#fillna()
+#fillna(value, inplace= True)
+
+import pandas as pd
+
+data = {
+    "Name": ['Sandesh', None, 'Abhishek', 'Marshall', 'Jermaine Cole', 'Mukesh', 'Suresh', 'Lokesh', 'Shiv', 'Hari'],
+    "Age": [21, None, 23, 50, 40, 60, 56, 34, 56, 22],
+    "Salary": [25000, None, 45000, 54000, 56000, 78000, 90000, 23000, 34000, 50000],
+    "Performance_Score": [85, None, 88, 90, 92, 98, 67, 89, 90, 98]
+}
+
+df = pd.DataFrame(data)
+print(df)
+
+# df.fillna(0, inplace=True)  #passing default value 0 where data is missing
+# print(df)
+
+
+#passing calculated value instead of default value
+
+# handling missing values using mean
+
+df['Age'].fillna(df['Age'].mean(),inplace=True)
+df['Salary'].fillna(df['Salary'].mean(),inplace=True)
+print(df)
